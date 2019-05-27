58611e4b85ac975f5b050fa666e12a1d9ec409d6
MRGHOSJ <bouzouitayassine@gmail.com>
2019-05-27 13:47:05 +0100

    Updates

---
+++ precache-manifest.d6f8316235b242f1c7c84c8746e82c73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a3ac006cbb74ca832ef",
+    "url": "/checkpointproject/static/js/runtime~main.9a3ac006.js"
+  },
+  {
+    "revision": "71ca9e0c036944fdbebd",
+    "url": "/checkpointproject/static/js/main.71ca9e0c.chunk.js"
+  },
+  {
+    "revision": "3cbaec28ac67fa279e0b",
+    "url": "/checkpointproject/static/js/1.3cbaec28.chunk.js"
+  },
+  {
+    "revision": "71ca9e0c036944fdbebd",
+    "url": "/checkpointproject/static/css/main.4943321c.chunk.css"
+  },
+  {
+    "revision": "102e890c3c303ab002ef5976ba236555",
+    "url": "/checkpointproject/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/MRGHOSJ/checkpointproject.git/precache-manifest.da78ec21a828f74d4f1d0f3d82107bba.js"
+  "/checkpointproject/precache-manifest.d6f8316235b242f1c7c84c8746e82c73.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/MRGHOSJ/checkpointproject.git/index.html", {
+workbox.routing.registerNavigationRoute("/checkpointproject/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
